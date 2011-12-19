500135902a8dab98369264cb280d1a952dc92215
Antoine Beaupré <anarcat@koumbit.org>
2011-12-19 12:44:11 -0500

    split into multiple classes to be able to install only the backend

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,26 @@
 class aegir {
+  include aegir::frontend
+}
+
+class aegir::frontend {
+  include aegir::apt
 
   package { 'aegir':
     ensure       => present,
     responsefile => 'files/aegir.preseed',
-    require      => Class['aegir::dependencies'],
   }
+}
 
+class aegir::backend {
+  include aegir::apt
+
+  package { 'aegir-provision': ensure => present }
+}
+
+
+class aegir::apt
   include apt
 
   apt::sources_list { "aegir-stable": content => "deb http://debian.aegirproject.org stable main" }
   apt::keys::key { "aegir": source => "puppet:///koumbit/debian.aegirproject.org.key" }
-
 }
