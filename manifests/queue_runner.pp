005f25141e5a77c8718e327683cc54e8e70592b9
Christopher Gervais <chris@ergonlogic.com>
2012-02-18 16:28:17 -0500

    Issue #1380358: Refactored queue-runner.pp, which now runs as a service.

--- manifests/queue_runner.pp
+++ manifests/queue_runner.pp
@@ -1,34 +1,46 @@
 class aegir::queue_runner {
 
-  include drush
+# Set some defaults
+  if ! $aegir_user       { $aegir_user = 'aegir' }
+  if ! $aegir_root       { $aegir_root = '/var/aegir' }
+  if ! ($aegir_version or $aegir_dev_build) { $aegir_version = '6.x-1.6' }
+  elsif $aegir_dev_build { $aegir_version = '6.x-1.x' }
+  if ! $aegir_site { $aegir_site = $fqdn }
+
+  Exec { path => '/usr/bin:/bin:/usr/sbin', user => $aegir_user, group => $aegir_user, }
+
+  if ($aegir_manual_build or $aegir_dev_build) { $aegir_installed = Exec['Hostmaster install'] }
+  else { $aegir_installed = Package['aegir'] }
+
 
   drush::dl { 'hosting_queue_runner':
-    site_alias => '@hostmaster',
-    cwd        => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
-    require    => Class['aegir'],
+    site_path  => "${aegir_root}/hostmaster-${aegir_version}/sites/${aegir_site}",
+    log        => "${aegir_root}/drush.log",
+    require    => $aegir_installed,
+    notify     => Drush::En['hosting_queue_runner'],
   }
 
   drush::en { 'hosting_queue_runner':
-    site_alias => '@hostmaster',
-    cwd        => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
-    require    => Drush::Dl["hosting_queue_runner"],
+    site_path  => "${aegir_root}/hostmaster-${aegir_version}/sites/${aegir_site}",
+    log        => "${aegir_root}/drush.log",
+    require    => Drush::Dl['hosting_queue_runner'],
   }
 
-  exec {"cp-script":
-    path    => '/usr/bin:/bin',
-    command => "cp /var/aegir/hostmaster-6.x-1.5/sites/aegir.local/modules/hosting_queue_runner/init.d.example /etc/init.d/hosting-queue-runner",
-    require => Drush::Dl["hosting_queue_runner"],
+  file {'hosting-queue-runner init script':
+    source  => "${aegir_root}/hostmaster-${aegir_version}/sites/${aegir_site}/modules/hosting_queue_runner/init.d.example",
+    path    => "/etc/init.d/hosting-queue-runner",
+    mode    => '755',
+    ensure  => present,
+    require => Drush::Dl['hosting_queue_runner'],
   }
 
-  exec {"symlinks-runlevels":
-    command => "/usr/sbin/update-rc.d hosting-queue-runner defaults",
-    require => Exec["cp-script"],
-  }
-
-  exec {"run-script":
-    path    => '/usr/bin:/bin',
-    command => "/etc/init.d/hosting-queue-runner start",
-    require => Exec["symlinks-runlevels"],
+  service {'hosting-queue-runner':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['hosting-queue-runner init script'],
+    require   => Drush::En['hosting_queue_runner'],
+    # TODO: remove this line once http://drupal.org/node/1404226 is fixed
+    status    => 'ps aux | grep hosting-queue-runner',
   }
 
 }
