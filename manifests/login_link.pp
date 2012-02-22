e3669ac5a8d94bfaedb28f1b5efdab2194fd7ef1
Antoine Beaupré <anarcat@koumbit.org>
2012-02-22 16:25:05 -0500

    we need to inherit defaults otherwise they don't work

--- manifests/login_link.pp
+++ manifests/login_link.pp
@@ -1,12 +1,10 @@
-class aegir::login_link {
+class aegir::login_link inherits aegir::defaults {
 
   if $aegir_force_login_link { $refreshonly = false }
   else { $refreshonly = true }
 
   if ! $aegir_user { $aegir_user = 'aegir' }
   if ! $aegir_root { $aegir_root = '/var/aegir' }
-
-  include aegir::defaults
 
   exec {'login link':
     command => 'drush @hostmaster uli',
