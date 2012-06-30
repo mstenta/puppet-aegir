7bd257223c45f64141b859d6ac5a0524ba602be7
Christopher Gervais <chris@ergonlogic.com>
2012-06-30 16:26:37 -0400

    Always use drush make, instead of letting the front-end try to handle it.

--- manifests/platform.pp
+++ manifests/platform.pp
@@ -18,30 +18,25 @@ define aegir::platform ($makefile, $force_complete = false, $working_copy = fals
     require => $aegir_installed,
   }
 
-  if ($force_complete or $working_copy) {
-    if $force_complete { $force_opt = ' --force-complete' }
-    if $working_copy { $working_opt = ' --working-copy' }
+  if $force_complete { $force_opt = ' --force-complete' }
+  if $working_copy { $working_opt = ' --working-copy' }
 
-    # we need to run drush make (and not verify) in order to override
-    # the drush make settings, because provision-verify won't take the
-    # --working-copy or --force-complete settings and pass them to
-    # drush make. hosting-import (below) will make the frontend run
-    # provision-verify through the queue eventually anyways.    
-    exec {"drush make ${name}":
-      command => "drush make $makefile ${name} $force_opt $working_opt",
-      creates => "${aegir_root}/platforms/${name}",
-      cwd     => "${aegir_root}/platforms",
-      require => Exec["provision-save-${name}"],
-      notify  => Exec["hosting-import-${name}"],
-    }
-
+  # we need to run drush make (and not verify) in order to override
+  # the drush make settings, because provision-verify won't take the
+  # --working-copy or --force-complete settings and pass them to
+  # drush make. hosting-import (below) will make the frontend run
+  # provision-verify through the queue eventually anyways.    
+  exec {"drush make ${name}":
+    command => "drush make $makefile ${name} $force_opt $working_opt",
+    creates => "${aegir_root}/platforms/${name}",
+    cwd     => "${aegir_root}/platforms",
+    require => Exec["provision-save-${name}"],
+    notify  => Exec["hosting-import-${name}"],
   }
 
   exec {"hosting-import-${name}":
     command => "drush @hostmaster hosting-import @platform_${name}",
-    require => [ Exec["provision-save-${name}"], 
-                 Class['aegir::frontend'],
-               ],
+    require => Exec["drush make ${name}"],
     refreshonly => true,
   }
                           
