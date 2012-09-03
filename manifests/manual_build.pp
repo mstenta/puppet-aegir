43733a491d128f3fab7e3fe128b1d068db61c892
Christopher Gervais <chris@ergonlogic.com>
2012-09-03 14:35:20 -0400

    Don't depend on apt::backports from a dependency.

--- manifests/manual_build.pp
+++ manifests/manual_build.pp
@@ -76,6 +76,7 @@ class aegir::manual_build::frontend {
   if ! $aegir_hostmaster_url { $aegir_hostmaster_url = $fqdn }
 
   # Ref.: http://community.aegirproject.org/installing/manual#Install_system_requirements
+  include apt::backports
   package { ['apache2', 'php5', 'php5-cli', 'php5-gd', 'php5-mysql', 'postfix', 'sudo', 'rsync',/* 'git-core',*/ 'unzip']:
     ensure  => present,
     require => Exec['update_apt'],
