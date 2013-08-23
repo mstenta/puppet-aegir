813df7bc73852bcad678e26818b7416e392213f9
Michael Stenta <mike@mstenta.net>
2013-08-23 14:16:08 -0400

    Issue #2072873 by m.stenta: Fixed Aegir::Extras doesn't work due to old variables.

--- manifests/extras.pp
+++ manifests/extras.pp
@@ -1,7 +1,6 @@
 class aegir::extras (
   $tasks_extra_version      = '6.x-2.0-alpha2',
   $registry_rebuild_version = '7.x-1.9',
-  $aegir_version            = $aegir::defaults::api2_vers
   ) inherits aegir::defaults {
 
   drush::dl { 'registry_rebuild':
@@ -14,7 +13,6 @@ class aegir::extras (
 
   drush::dl { 'hosting_tasks_extra':
     version   => $tasks_extra_version,
-    site_path => "${aegir::defaults::aegir_root}/hostmaster-${aegir_version}/sites/${aegir::defaults::frontend_url}",
   }
 
   drush::en { 'hosting_tasks_extra': }
