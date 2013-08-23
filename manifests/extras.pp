174a454166d9dee529d0b85a50f1d0b2bd43bbd1
Michael Stenta <mike@mstenta.net>
2013-08-23 17:33:32 -0400

    Remove 'version' arguments from drush::dl in aegir::extras.

--- manifests/extras.pp
+++ manifests/extras.pp
@@ -1,21 +1,14 @@
-class aegir::extras (
-  $tasks_extra_version      = '6.x-2.0-alpha2',
-  $registry_rebuild_version = '7.x-1.9',
-  ) inherits aegir::defaults {
+class aegir::extras inherits aegir::defaults {
 
   drush::dl { 'registry_rebuild':
     type => 'extension',
-    version => $registry_rebuild_version,
   }
 
   drush::dl { 'provision_tasks_extra':
     type => 'extension',
-    version => $tasks_extra_version,
   }
 
-  drush::dl { 'hosting_tasks_extra':
-    version   => $tasks_extra_version,
-  }
+  drush::dl { 'hosting_tasks_extra': }
 
   drush::en { 'hosting_tasks_extra': }
 
