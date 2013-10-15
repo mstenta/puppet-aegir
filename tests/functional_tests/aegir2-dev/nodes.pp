f07056ced8b2165b234489a747812a72b7e516f5
Herman van Rink <rink@initfour.nl>
2013-10-15 22:25:20 +0200

    typo fix

--- tests/functional_tests/aegir2-dev/nodes.pp
+++ tests/functional_tests/aegir2-dev/nodes.pp
@@ -1,13 +1,13 @@
 node "aegir2" {
 
   class { 'drush::git::drush' :
-    #$git_branch = '5.x',
-    $git_tag    = '5.10',
+    #$git_branch => '5.x',
+    git_tag    => '5.10',
   }
 
   class { 'aegir::dev' :
-    $hostmaster_ref = '6.x-2.x',
-    $provision_ref  = '6.x-2.x',
+    hostmaster_ref => '6.x-2.x',
+    provision_ref  => '6.x-2.x',
     require => Class['drush'],
   }
 
