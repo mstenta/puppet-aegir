1508c758c0492537a6da0333089121b04aca21de
Christopher Gervais <chris@ergonlogic.com>
2013-10-17 10:38:13 -0400

    Add explicit platform_path for testing.

--- tests/functional_tests/aegir2-dev/nodes.pp
+++ tests/functional_tests/aegir2-dev/nodes.pp
@@ -1,14 +1,16 @@
 node "aegir2-dev.test" {
 
   class { 'drush::git::drush' :
-    #git_branch => '5.x',
-    git_tag    => '5.10.0',
+    git_branch => '5.x',
+    #git_tag    => '5.10.0',
   }
 
   class { 'aegir::dev' :
     hostmaster_ref => '6.x-2.x',
     provision_ref  => '6.x-2.x',
-    require => Class['drush::git::drush'],
+    # This shouldn't be necessary. See: https://drupal.org/node/2114025.
+    platform_path  => '/var/aegir/hostmaster-6.x-2.x',
+    require        => Class['drush::git::drush'],
   }
 
 }
