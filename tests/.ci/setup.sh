1a46713a29eb0a26cfad3d9dc6c2073a3a99904b
Herman van Rink <rink@initfour.nl>
2013-10-23 14:26:10 +0200

    Quote SCRIPT_DIR to account for spaces

--- tests/.ci/setup.sh
+++ tests/.ci/setup.sh
@@ -5,12 +5,12 @@
 CWD=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ ! -h $SCRIPT_DIR/../../Vagrantfile ]
+if [ ! -h "$SCRIPT_DIR/../../Vagrantfile" ]
 then
-  ln -s $SCRIPT_DIR/Vagrantfile $SCRIPT_DIR/../../Vagrantfile
+  ln -s "$SCRIPT_DIR/Vagrantfile" "$SCRIPT_DIR/../../Vagrantfile"
 fi
 
-if [ -e $SCRIPT_DIR/.vagrant ]
+if [ -e "$SCRIPT_DIR/.vagrant" ]
 then
   cd $CWD && vagrant destroy --force
 fi
