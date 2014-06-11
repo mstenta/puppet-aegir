7b8182b8d43dc04e5442760bc39c638755d7542d
Herman van Rink <rink@initfour.nl>
2014-06-11 18:09:29 +0200

    Issue #2114025 by helmo: Fixed empty --root option for hostmaster-install.

--- tests/functional_tests/aegir3-dev/nodes.pp
+++ tests/functional_tests/aegir3-dev/nodes.pp
@@ -8,8 +8,7 @@ node "aegir3-dev.test" {
   class { 'aegir::dev' :
     hostmaster_ref => '7.x-3.x',
     provision_ref  => '7.x-3.x',
-    # This shouldn't be necessary. See: https://drupal.org/node/2114025.
-    #platform_path  => '/var/aegir/hostmaster-6.x-2.x',
+    platform_path  => '/var/aegir/hostmaster-7.x-3.x',
     require        => Class['drush::git::drush'],
   }
 
