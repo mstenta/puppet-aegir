f656273a0781e42155f80d404d2acfdf1dbdec61
Herman van Rink <rink@initfour.nl>
2015-05-04 14:38:46 +0200

    Let the drush output be a bit wider then 80 chars ... 130 for now.

--- tests/functional_tests/aegir3-drush-master-dev/run-tests.sh
+++ tests/functional_tests/aegir3-drush-master-dev/run-tests.sh
@@ -5,14 +5,14 @@ then
   exit 1
 fi
 
-vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"drush -y @hostmaster --debug provision-tests-run\"'"
+vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"COLUMNS=130 drush -y @hostmaster --debug provision-tests-run\"'"
 if [ "$?" -ne "0" ]
 then
   echo "'provision-tests-run' failed. Leaving vm in place for forensic analysis."
   exit 1
 fi
 
-vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"drush -y @hostmaster --debug provision-tests-new-run\"'"
+vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"COLUMNS=130 drush -y @hostmaster --debug provision-tests-new-run\"'"
 if [ "$?" -ne "0" ]
 then
   echo "'provision-tests-run' failed. Leaving vm in place for forensic analysis."
