eddf7b9e47ab85612501718d218bbdcc058e4e69
Christopher Gervais <chris@ergonlogic.com>
2015-08-21 15:45:18 -0400

    Allow for other branches.

--- tests/functional_tests/aegir3-unstable/nodes.pp
+++ tests/functional_tests/aegir3-unstable/nodes.pp
@@ -1,13 +1,8 @@
 node "aegir3-unstable" {
 
-  class { 'drush' :
-    api  => 6,
-  }
-
   class { 'aegir' :
     api     => 3,
     dist    => 'unstable',
-    require => Class['drush'],
   }
 
 }
--- tests/functional_tests/aegir3-unstable/setup.sh
+++ tests/functional_tests/aegir3-unstable/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DRUSH_BRANCH='1.0.x'
+AEGIR_BRANCH='unstable'
 CWD=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -12,14 +14,14 @@ if [ -d $SCRIPT_DIR/modules/drush ]
 then
   cd $SCRIPT_DIR/modules/drush && git pull
 else
-  git clone --branch 1.0.x http://git.drupal.org/project/puppet-drush.git $SCRIPT_DIR/modules/drush
+  git clone --branch $DRUSH_BRANCH http://git.drupal.org/project/puppet-drush.git $SCRIPT_DIR/modules/drush
 fi
 
 if [ -d $SCRIPT_DIR/modules/aegir ]
 then
   cd $SCRIPT_DIR/modules/aegir && git pull
 else
-  git clone --branch 1.0.x http://git.drupal.org/project/puppet-aegir.git $SCRIPT_DIR/modules/aegir
+  git clone --branch $AEGIR_BRANCH http://git.drupal.org/project/puppet-aegir.git $SCRIPT_DIR/modules/aegir
 fi
 
 if [ ! -h $SCRIPT_DIR/../../../Vagrantfile ]
