48ca2b10b208e62a41887108e412e09b72239840
Christopher Gervais <chris@ergonlogic.com>
2015-08-21 15:16:00 -0400

    Clean up.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,31 +13,6 @@ class aegir (
   $ensure       = $aegir::defaults::ensure
   ) inherits aegir::defaults {
 
-
-
-
-  case $api {
-    1: {
-      $real_api = ''
-    }
-    2: {
-      $real_api = 2
-      package { 'aegir-provision':
-        ensure => absent;
-      }
-    }
-    3, '', default: {
-      $real_api = 3
-      package { [ 'aegir2-provision', 'aegir-provision']:
-        ensure => absent;
-      }
-      if !($api in $apis) {
-        warning("'${api}' is not a valid Aegir API version. Values can be '1', '2' or '3'. Defaulting to '3'.")
-      }
-    }
-  }
-
-
   $apis = ['1', '2', '3']
 
   case $api {
