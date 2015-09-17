bcc97672d05cbbdfe685ce6197a49dd141a58b5b
Christopher Gervais <chris@ergonlogic.com>
2015-09-17 18:40:45 -0400

    clean up test script.

--- tests/functional_tests/aegir3-dev-drush7/run-tests.sh
+++ tests/functional_tests/aegir3-dev-drush7/run-tests.sh
@@ -1,10 +1,3 @@
-#vagrant up --provision
-if [ "$?" -ne "0" ]
-then
-  echo "'vagrant up' failed. Leaving vm in place for forensic analysis."
-#  exit 1
-fi
-
 vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"drush -y @hostmaster provision-tests-run\"'"
 if [ "$?" -ne "0" ]
 then
@@ -12,5 +5,5 @@ then
   exit 1
 fi
 
-#vagrant destroy --force
+vagrant destroy --force
 exit 0
