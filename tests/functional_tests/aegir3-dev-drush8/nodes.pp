5b79e51d4d675bbef814f740736f97f55b267a66
Christopher Gervais <chris@ergonlogic.com>
2015-09-17 22:57:28 -0400

    Add test on Drush 8.

---
+++ tests/functional_tests/aegir3-dev-drush8/nodes.pp
@@ -0,0 +1,24 @@
+node "aegir3-dev-drush8.test" {
+
+  class { 'drush::git::drush' :
+    #git_branch => '8.x',
+    git_branch => 'master',
+  }
+
+  class { 'aegir::dev' :
+    hostmaster_ref => '7.x-3.x',
+    provision_ref  => '7.x-3.x',
+    platform_path  => '/var/aegir/hostmaster-7.x-3.x',
+    require        => Class['drush::git::drush'],
+    start_queued_service => false,
+  }
+
+  drush::run { 'Clear Drush caches for Aegir':
+    command    => 'cache-clear',
+    site_alias => '@hostmaster',
+    arguments  => 'all',
+    drush_user => 'aegir',
+    drush_home => '/var/aegir',
+    require    => Class['aegir::dev'],
+  }
+}
---
+++ tests/functional_tests/aegir3-dev-drush8/run-tests.sh
@@ -0,0 +1,9 @@
+vagrant ssh -c "sudo -u root -s /bin/bash -c 'su aegir -l -c \"drush -y @hostmaster provision-tests-run\"'"
+if [ "$?" -ne "0" ]
+then
+  echo "'provision-tests-run' failed. Leaving vm in place for forensic analysis."
+  exit 1
+fi
+
+vagrant destroy --force
+exit 0
