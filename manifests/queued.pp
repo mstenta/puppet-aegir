ae011e6946293f0abe7a39b9d0d38fa0914ce0f0
Christopher Gervais <chris@ergonlogic.com>
2016-02-08 11:19:46 -0500

    Issue #2664308 by helmo: Add hosting_queued systemd service file.

--- manifests/queued.pp
+++ manifests/queued.pp
@@ -9,11 +9,18 @@ class aegir::queued inherits aegir::defaults {
     owner   => 'root', group => 'root', mode => '0755',
   }
 
+  file {'hosting-queued systemd service file':
+    ensure  => present,
+    source  => 'puppet:///modules/aegir/aegir3-hostmaster.hosting-queued.service',
+    path    => '/lib/systemd/system/hosting-queued.service',
+    owner   => 'root', group => 'root', mode => '0755',
+  }
+
   service {'hosting-queued':
     ensure    => running,
     enable    => true,
     subscribe => File['hosting-queued init script'],
-    require   => Drush::En['hosting_queued'],
+    require   => [ Drush::En['hosting_queued'], File['hosting-queued systemd service file'] ],
   }
 
 }
